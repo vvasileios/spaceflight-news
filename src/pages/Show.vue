b01413122bb7dc98540350233416172afc24bfa2
Vasileios <vtvasileiadis@gmail.com>
2024-04-01 21:18:16 +0300

    minor style change

--- src/pages/Show.vue
+++ src/pages/Show.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="flex items-center justify-center mt-10">
-    <div class="bg-white-200 shadow-xl border-2 p-8 max-w-sm">
+  <div class="flex items-center justify-center mt-60">
+    <div class="bg-white-200 shadow-xl border-2 p-8 max-w-xl">
       <h2 class="text-xl font-bold mb-4">{{ article.title }}</h2>
-      <img v-if="article.image_url" :src="article.image_url" class="mb-4" />
+      <img
+        v-if="article.image_url"
+        :src="article.image_url"
+        class="mb-10 mt-10"
+      />
       <p class="mb-4">{{ article.summary }}</p>
       <a
         :href="article.url"
         target="_blank"
         rel="noopener noreferrer"
-        class="text-blue-500 hover:underline"
-        >Open Article</a
+        class="text-blue-500 hover:cursor-pointer hover:underline"
       >
+        Open Article
+      </a>
     </div>
   </div>
 </template>
