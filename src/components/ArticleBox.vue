c3ffe241347b07b800879f7c6ee3eaae3a06752a
Vasileios <vtvasileiadis@gmail.com>
2024-04-26 22:34:36 +0300

    make box component for singular article

--- src/components/ArticleBox.vue
+++ src/components/ArticleBox.vue
@@ -1,3 +1,55 @@
-<script setup></script>
+<script setup>
+import { dateFormatter, timeFormatter } from "../utils/formatter.js";
 
-<template></template>
+defineProps({
+  singleArticle: Object,
+});
+
+const emit = defineEmits(["goBack", "fullArticle"]);
+
+const goBack = () => emit("goBack");
+
+const hyperLink = (url) => emit("fullArticle", url);
+</script>
+
+<template>
+  <div
+    class="container flex flex-col relative mx-auto my-10 border shadow-xl rounded-xl"
+  >
+    <div class="flex flex-col justify-center items-center">
+      <h1 class="font-bold text-sm md:text-xl my-3">
+        {{ singleArticle.news_site }}
+      </h1>
+      <h2 class="font-medium text-xs md:text-lg">{{ singleArticle.title }}</h2>
+      <div class="w-2/3 my-5">
+        <img
+          :src="singleArticle.image_url"
+          class="rounded-2xl"
+          alt="Image of Article"
+        />
+      </div>
+      <div class="flex gap-2 mb-5">
+        <p class="font-semibold text-xs text-slate-600">
+          {{ dateFormatter(singleArticle.published_at) }}
+        </p>
+        <p class="font-semibold text-xs text-slate-600">
+          {{ timeFormatter(singleArticle.published_at) }}
+        </p>
+      </div>
+    </div>
+    <div class="flex justify-between mx-10 mb-5">
+      <button
+        class="bg-transparent hover:bg-blue-700 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        @click="goBack"
+      >
+        Back
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="hyperLink(singleArticle.url)"
+      >
+        Full Article
+      </button>
+    </div>
+  </div>
+</template>
--- src/pages/ArticleShow.vue
+++ src/pages/ArticleShow.vue
@@ -2,7 +2,7 @@
 import { onMounted, computed } from "vue";
 import { useStore } from "vuex";
 import { useRouter, useRoute } from "vue-router";
-import { dateFormatter, timeFormatter } from "../utils/formatter.js";
+import ArticleBox from "../components/ArticleBox.vue";
 
 const store = useStore();
 const router = useRouter();
@@ -12,47 +12,15 @@ onMounted(() => store.dispatch("fetchArticle", route.params.id));
 
 const article = computed(() => store.getters.getArticle);
 
-const goBack = () => router.push({ name: "home" });
+const handleBack = () => router.push({ name: "home" });
 
-const hyperLink = (link) => window.open(link, "_blank");
+const handleFullArticle = (link) => window.open(link, "_blank");
 </script>
 
 <template>
-  <div
-    class="container flex flex-col relative mx-auto my-10 border shadow-xl rounded-xl"
-  >
-    <div class="flex flex-col justify-center items-center">
-      <h1 class="font-bold text-sm md:text-xl my-3">{{ article.news_site }}</h1>
-      <h2 class="font-medium text-xs md:text-lg">{{ article.title }}</h2>
-      <div class="w-2/3 my-5">
-        <img
-          :src="article.image_url"
-          class="rounded-2xl"
-          alt="Image of Article"
-        />
-      </div>
-      <div class="flex gap-2 mb-5">
-        <p class="font-semibold text-xs text-slate-600">
-          {{ dateFormatter(article.published_at) }}
-        </p>
-        <p class="font-semibold text-xs text-slate-600">
-          {{ timeFormatter(article.published_at) }}
-        </p>
-      </div>
-    </div>
-    <div class="flex justify-between mx-10 mb-5">
-      <button
-        class="bg-transparent hover:bg-blue-700 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        @click="goBack"
-      >
-        Back
-      </button>
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        @click="hyperLink(article.url)"
-      >
-        Full Article
-      </button>
-    </div>
-  </div>
+  <ArticleBox
+    :single-article="article"
+    @goBack="handleBack"
+    @fullArticle="handleFullArticle"
+  />
 </template>
